5a921e0b53dda2ba4345d454ba23d8931aea378f
David Fornazier <david.fornazier@gmail.com>
2021-11-03 16:17:38 -0300

    registro de saldo ok

--- transferenciabancariaapi/broker/broker.go
+++ transferenciabancariaapi/broker/broker.go
@@ -51,7 +51,7 @@ func InitBroker() {
 	log.Println("Banco de dados inicializado")
 }
 
-func GetSaldo(saldo models.Saldo) (*[]models.Saldo, error) {
+func GetSaldo(saldo models.Saldo) ([]models.Saldo, error) {
 	ctx := context.Background()
 	cn, _ := Pool.Acquire(ctx)
 	defer cn.Release()
@@ -76,12 +76,97 @@ func RegistraSaldo(rs models.RegistroSaldo) error {
 	cn, _ := Pool.Acquire(ctx)
 	defer cn.Release()
 
+	com, err := cn.Exec(ctx, sqlRegistraSaldo(), rs.TitularId, rs.ContaId, rs.Sinal, rs.Valor)
+	if err != nil {
+		log.Println("Erro no SQL \"RegistraSaldo\":", err.Error())
+		return err
+	}
+
+	if com.RowsAffected() > 0 {
+		log.Println("Registro salvo com sucesso.")
+	}
+
 	go atualizaSaldo(rs)
+
 	return nil
 }
 
 func atualizaSaldo(rs models.RegistroSaldo) {
+	log.Printf("Iniciando rotina de atualização de saldo para: %d - %d\n", rs.TitularId, rs.ContaId)
 
+	var saldo models.Saldo
+
+	aux, err := GetSaldo(models.Saldo{
+		TitularId: rs.TitularId,
+		ContaId:   rs.ContaId,
+	})
+	if err != nil {
+		log.Println("Erro ao resgatar Saldo:", err.Error())
+	}
+
+	saldo = aux[0]
+
+	err = saldo.ErroGetSaldo()
+	if err != nil {
+		log.Println("Cliente não possui registro de saldo válido")
+		return
+	}
+
+	res, err := getAllRegistrosSaldo(rs)
+	if err != nil {
+		log.Printf("Falha ao atualizar saldo de %d - %d\n", rs.TitularId, rs.ContaId)
+		return
+	}
+	var novoSaldo float64
+	for i := 0; i < len(res); i++ {
+		registro := res[i]
+
+		if registro.Sinal == 0 { //credita o valor
+			novoSaldo += registro.Valor
+		} else if registro.Sinal == 1 { //debita o valor
+			novoSaldo -= registro.Valor
+		}
+	}
+	//atualizando novo saldo
+	saldo.Saldo = novoSaldo
+	err = UpdateSaldo(saldo)
+	if err != nil {
+		log.Println("Não foi possível atualizado o saldo:", err.Error())
+	} else {
+		log.Printf("Saldo de %d - %d atualizado\n", saldo.TitularId, saldo.ContaId)
+	}
+}
+
+func getAllRegistrosSaldo(rs models.RegistroSaldo) ([]models.RegistroSaldo, error) {
+	ctx := context.Background()
+	cn, _ := Pool.Acquire(ctx)
+	defer cn.Release()
+
+	rows, err := cn.Query(ctx, sqlGetRegistrosSaldo(), rs.TitularId, rs.ContaId)
+	if err != nil {
+		log.Println("Erro no SQL \"getAllRegistrosSaldo\":", err.Error())
+		return nil, err
+	}
+
+	res := rowsToArrayRegistroSaldo(rows)
+
+	return res, nil
+}
+
+func UpdateSaldo(saldo models.Saldo) error {
+	ctx := context.Background()
+	cn, _ := Pool.Acquire(ctx)
+	defer cn.Release()
+	com, err := cn.Exec(ctx, sqlUpdateSaldo(), saldo.Saldo, saldo.TitularId, saldo.ContaId)
+	if err != nil {
+		log.Println("Erro no SQL \"RegistraSaldo\":", err.Error())
+		return err
+	}
+
+	if com.RowsAffected() > 0 {
+		log.Println("Registro salvo com sucesso.")
+	}
+	return nil
 }
 
 func RealizaTransferencia(tr models.Transferencia) error {
@@ -91,7 +176,7 @@ func RealizaTransferencia(tr models.Transferencia) error {
 	return nil
 }
 
-func rowsToArraySaldo(rows pgx.Rows) *[]models.Saldo {
+func rowsToArraySaldo(rows pgx.Rows) []models.Saldo {
 	var ret []models.Saldo
 	for rows.Next() {
 		conta := 0
@@ -109,10 +194,10 @@ func rowsToArraySaldo(rows pgx.Rows) *[]models.Saldo {
 			ret = append(ret, saldo)
 		}
 	}
-	return &ret
+	return ret
 }
 
-func rowsToArrayrRegistroSaldo(rows pgx.Rows) *[]models.RegistroSaldo {
+func rowsToArrayRegistroSaldo(rows pgx.Rows) []models.RegistroSaldo {
 	var ret []models.RegistroSaldo
 	for rows.Next() {
 		titular := 0
@@ -132,5 +217,5 @@ func rowsToArrayrRegistroSaldo(rows pgx.Rows) *[]models.RegistroSaldo {
 			ret = append(ret, rs)
 		}
 	}
-	return &ret
+	return ret
 }
--- transferenciabancariaapi/broker/sqls.go
+++ transferenciabancariaapi/broker/sqls.go
@@ -3,3 +3,16 @@ package broker
 func sqlGetSaldo() string {
 	return "select conta_id, titular_id, saldo from public.saldo where conta_id=$1 and titular_id=$2"
 }
+
+func sqlGetRegistrosSaldo() string {
+	return "select titular_id, conta_id, sinal, valor from public.registrosaldo where titular_id=$1 and conta_id=$1"
+}
+
+func sqlRegistraSaldo() string {
+	return "insert into public.registrosaldo (titular_id, conta_id, sinal, valor) values ($1,$2,$3,$4);"
+}
+
+func sqlUpdateSaldo() string {
+	// UPDATE public.tipos SET descricao='', metadado='', "status"=false, datacriacao=current_timestamp(), dataalteracao=current_timestamp() WHERE codigo='';
+	return "update public.saldo set saldo=$1 where titular_id=$2 and conta_id=$3"
+}
